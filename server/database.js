9ed310e471e3a8b7710729b516d9b59fe1ee68ba
Annie Chen <annie.chen@tufts.edu>
2017-02-23 16:08:47 -0500

    added foreign keys

--- server/database.js
+++ server/database.js
@@ -4,19 +4,57 @@ const client = pgp(connectionString);
 client.connect();
 
 var query_show = client.query(
-				'DROP TABLE IF EXISTS show');
+	'DROP TABLE IF EXISTS show');
+
 query_show = client.query(
-'CREATE TABLE show(name TEXT, showID INTEGER PRIMARY KEY, day INTEGER, hour INTEGER, length INTEGER, alternating BOOLEAN)');
+	'CREATE TABLE show(
+		name TEXT, 
+		showID INTEGER PRIMARY KEY, 
+		day INTEGER, 
+		hour INTEGER, 
+		length INTEGER, 
+		alternating BOOLEAN)');
+
 var query_dj = client.query(
-			      'DROP TABLE IF EXISTS dj');
+	'DROP TABLE IF EXISTS dj');
+
 query_dj = client.query(
-'CREATE TABLE dj(firstName TEXT, lastName TEXT, djName TEXT, tuftsID INTEGER PRIMARY KEY, volunteerHours INTEGER, active BOOLEAN)');
+	'CREATE TABLE dj(
+		firstName TEXT, 
+		lastName TEXT, 
+		djName TEXT, 
+		tuftsID INTEGER PRIMARY KEY, 
+		volunteerHours INTEGER, 
+		active BOOLEAN)');
+
 var query_djShow = client.query(
-				  'DROP TABLE IF EXISTS djShow');
+	'DROP TABLE IF EXISTS djShow');
+
 query_djShow = client.query(
-'CREATE TABLE djshow(tuftsID INTEGER, showID INTEGER)');
+	'CREATE TABLE djShow(
+		tuftsID INTEGER, 
+		showID INTEGER)');
+
 var query_volunteerLog = client.query(
-				      'DROP TABLE IF EXISTS volunteerLog');
+	'DROP TABLE IF EXISTS volunteerLog');
+
 query_volunteerLog = client.query(
-'CREATE TABLE volunteerLog(tuftsID INTEGER, date DATE, length INTEGER, approved BOOLEAN, task TEXT)');
+	'CREATE TABLE volunteerLog(
+		tuftsID INTEGER, 
+		date DATE, 
+		length INTEGER, 
+		approved BOOLEAN, 
+		task TEXT)');
+
+var query_add_foreign_key = client.query(
+	'ALTER TABLE djShow
+	 ADD FOREIGN KEY (tuftsID)
+	 REFERENCES dj(tuftsID)')
+
+query_add_foreign_key = client.query(
+	'ALTER TABLE volunteerLog
+	 ADD FOREIGN KEY (tuftsID)
+	 REFERENCES dj(tuftsID)')
+
+
 
