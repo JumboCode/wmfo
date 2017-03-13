cf025dc176c666776523fcc4dfbd7d95e3f97399
San Ozan Akdag <sanozan7@San-Akdags-MacBook-Pro-3.local>
2017-03-12 22:39:43 -0400

    updated Home and ShowForm, added styling

--- client/src/components/ShowForm.tsx
+++ client/src/components/ShowForm.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 export interface ShowFormProps { compiler: string; framework: string; }
+import {Link} from "react-router";
 //export interface DJNameFormProps {name: string;}
 //export interface ShowNameFormProps {name: string;}
 
@@ -10,11 +11,45 @@ export class ShowForm extends React.Component<ShowFormProps, any>
 {
     render (){
         return (
-                <div>
+                <div id="show_form">
+                    <Back/>
                     <DJNameForm/>
                     <ShowNameForm/>
                 </div>
             )
+    }
+}
+
+class Back extends React.Component<any, any>
+{
+    render(){
+        return (
+            <div>
+                <a href="">
+                          <img id="logo" src="https://www.wmfo.org/wp-content/uploads/2016/01/Logo-1.png" alt="WMFO"  />
+
+                        </a>
+
+                        <div id="phone">Call in! 855-915-WMFO</div>
+
+                        <ul>
+                          <li><a className ="active" href="#home">Home</a></li>
+                          <li><a href="#contact">Contact</a></li>
+                          <li><a href="#about">About</a></li>
+                        </ul>
+
+                        <div id="main">
+                          <div id="user"><h1>User Profile</h1>
+
+                            <li><Link to="/home">Home</Link></li>
+                                <li><Link to="/show_form">Show Form</Link></li>
+
+                           
+                          </div>
+                </div>
+            </div>
+
+        );
     }
 }
 
@@ -42,6 +77,7 @@ class DJNameForm extends React.Component<any, any>
       <form onSubmit={this.handleSubmit}>
         <label>
           DJ Name:
+          <br/>
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
@@ -75,6 +111,7 @@ class ShowNameForm extends React.Component<any, any>
       <form onSubmit={this.handleSubmit}>
         <label>
           Show Name:
+          <br/>
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
         <input type="submit" value="Submit" />
