d641da40b6e1fba76eeb9f68b70d7f2079a0a0c1
San Ozan Akdag <sanozan7@San-Akdags-MacBook-Pro-3.local>
2017-03-12 22:15:00 -0400

    page links together, show form to be added

--- client/src/components/Hello.tsx
+++ client/src/components/Hello.tsx
@@ -1,9 +1,37 @@
 import * as React from "react";
-
+import {Link} from "react-router";
 export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, undefined> {
     render() {
-        return <h1>Hello!</h1>;
+    	return (
+    		<div>
+						<a href="">
+						  <img id="logo" src="https://www.wmfo.org/wp-content/uploads/2016/01/Logo-1.png" alt="WMFO"  />
+
+						</a>
+
+						<div id="phone">Call in! 855-915-WMFO</div>
+
+						<ul>
+						  <li><a className ="active" href="#home">Home</a></li>
+						  <li><a href="#contact">Contact</a></li>
+						  <li><a href="#about">About</a></li>
+						</ul>
+
+						<div id="main">
+						  <div id="user"><h1>User Profile</h1>
+
+                			<li><Link to="/hello">Hello</Link></li>
+               			 	<li><Link to="/show_form">Show Form</Link></li>
+
+						   
+						  </div>
+						  <div id="studio">
+						    <img id="studio_pic" src="https://www.wmfo.org/wp-content/uploads/2017/02/WmfoStudioAMainBoardPano.2016.04.01.CC-By-EdwardBeuchert.Large_-1.jpg" alt="Studio"/>
+						  </div>
+						</div>
+			</div>
+		);
     }
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,31 +1,29 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Router, Route, Link, browserHistory, IndexRoute } from "react-router"
+import { Router, Route, browserHistory, IndexRoute } from "react-router"
 
 import { Hello } from "./components/Hello";
 import { About } from "./components/About";
+import { ShowForm } from "./components/ShowForm"
 import { Unknown } from "./components/Unknown";
 
 class App extends React.Component<undefined, undefined> {
     render() {
         return (
             <div>
-            <h1>App</h1>
-            <ul>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/hello">Hello</Link></li>
-            </ul>
             {this.props.children}
             </div>
         )
     }
 };
 ReactDOM.render((
+
     <Router history={browserHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={Hello} />
             <Route path="about" component={About}/>
             <Route path="hello" component={Hello}/>
+            <Route path="show_form" component={ShowForm}/>
             <Route path="*" component={Unknown}/>
         </Route>
     </Router>
