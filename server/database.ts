a7b943b14f5e876ba0f340082991175f5b5939d5
Nicholas Metzger <nicholas.metzger@tufts.edu>
2017-04-02 22:12:04 -0400

    added not-working getshow query

--- server/database.ts
+++ server/database.ts
@@ -13,6 +13,8 @@ interface dj {
 
 /* DJ TABLE */
 /* Takes a first name string of a dj and returns the id of the dj */
+
+/*
 function getUserID(firstName: string): Promise<number> {
     return db.one('SELECT tuftsid FROM dj WHERE firstName = $1', firstName);
 }
@@ -21,7 +23,6 @@ function getDj(tuftsID: number): Promise<string> {
     return db.one('SELECT firstName FROM dj WHERE tuftsID = $1', tuftsID);
 }
 
-/*
 function insertDj(firstName: string, lastName: string, djName: string, tuftsID: number, volunteerHours: number, active: boolean) {
     return db.none('INSERT INTO dj(firstName, lastName, djName, tuftsID, volunteerHours, active) VALUES($1, $2, $3, $4, $5, $6)', [firstName, lastName, djName, tuftsID, volunteerHours, active])
     .then(() => {console.log("//////WE INSERTED NEW DJ")});
@@ -31,15 +32,26 @@ function insertDj(firstName: string, lastName: string, djName: string, tuftsID: 
 /* INSERTING DATA TO SHOW TABLE */
 /* Takes the name of the show and the id of the show and returns nothing */
 /*
-function insertShow(name:string, id: number) {
-    return db.none('INSERT INTO show(name, showID) VALUES($1, $2)', [name, id])
+function insertShow(name:string, showID: number, day: number, hour: number, length: number, alternating: boolean) {
+    return db.none('INSERT INTO show(name, showid, day, hour, length, alternating) VALUES($1, $2, $3, $4, $5, $6)', [name, showID, day, hour, length, alternating])
     .then(() => {console.log("//////////inserted show")});
 
 }
 */
+function getShow(id: number) {
+    return db.one('SELECT name FROM show  WHERE id = $1', id);
+}
+export async function printing() {
+  try {
+    //await insertShow('WhoIsGoingToWinThePineapple', 1, 1, 1, 1, true);
+    const name: string = await getShow(1);
+    console.log(name);
+  } catch(e){
+    console.log("'exception: ' e");
+  }
+}
 
-
-
+/*
 export async function printing() {
   try {
     const x: number = await getUserID('Mac');
@@ -49,8 +61,8 @@ export async function printing() {
   } catch(e){
     console.log("'exception: ' e");
   }
-
-  //await insertDj('Nick', 'Metz', 'NickyMetz', 69, 9, true);
+ // insertDj('Nick', 'Metzger', 'NickyMetz', 69, 9, true);
  // await insertShow('Annie', 618);
 }
+*/
 
