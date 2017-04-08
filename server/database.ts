6c4b3d696d5d17982b14d023fea8540ffe8fc6e7
Marilyn Sun <19marilyns@gmail.com>
2017-04-08 13:54:22 -0400

    added vlog insert/get

--- server/database.ts
+++ server/database.ts
@@ -14,15 +14,15 @@ interface dj {
 /* DJ TABLE */
 /* Takes a first name string of a dj and returns the id of the dj */
 
-/*
 function getUserID(firstName: string): Promise<number> {
     return db.one('SELECT tuftsid FROM dj WHERE firstName = $1', firstName);
-}
+} 
 
 function getDj(tuftsID: number): Promise<string> {
     return db.one('SELECT firstName FROM dj WHERE tuftsID = $1', tuftsID);
 }
 
+/*
 function insertDj(firstName: string, lastName: string, djName: string, tuftsID: number, volunteerHours: number, active: boolean) {
     return db.none('INSERT INTO dj(firstName, lastName, djName, tuftsID, volunteerHours, active) VALUES($1, $2, $3, $4, $5, $6)', [firstName, lastName, djName, tuftsID, volunteerHours, active])
     .then(() => {console.log("//////WE INSERTED NEW DJ")});
@@ -38,23 +38,38 @@ function insertShow(name:string, showID: number, day: number, hour: number, leng
 
 }
 */
+
+/*
 function getShow(id: number) {
     return db.one('SELECT name FROM show WHERE showID = $1', id);
 }
-export async function printing() {
-  try {
-  //  await insertShow('WhoIsGoingToWinThePineapple', 1, 1, 1, 1, true);
-    const name: string = await getShow(1);
-    console.log(name);
-  } catch(e){
-    console.log("'exception: ' e");
-  }
+*/
+
+
+/* INSERTING VOLUNTEER LOG */
+
+function insertVlog(tuftsID: number, date: Date, length: number, approved: boolean, task: string){
+    return db.none('INSERT INTO volunteerlog(tuftsID, date, length, approved, task) VALUES($1, $2, $3, $4, $5)', [tuftsID,
+    date, length, approved, task])
+    .then(() => {console.log("//////inserted vlog")});
 }
 
-/*
+
+/* Return's all dates of vlogs under same tuftsID */
+function getVlog(tuftsID: number): Promise<Date> {
+    return db.any('SELECT date FROM volunteerlog WHERE tuftsID = $1', tuftsID);
+}
+
+
+var b: Date = new Date('2015-11-16T00:00:00');
+
 export async function printing() {
   try {
-    const x: number = await getUserID('Mac');
+  //  insertDj('Marilyn', 'Miller', 'Maac', 234, 1, true);
+    insertVlog(123, b, 60, true, 'play music');
+    const y: number = await getUserID('Mac');
+    console.log(y); 
+    const x: Date = await getVlog(123);
     console.log(x);
     const name: string = await getDj(12345);
     console.log(name);
@@ -64,5 +79,5 @@ export async function printing() {
  // insertDj('Nick', 'Metzger', 'NickyMetz', 69, 9, true);
  // await insertShow('Annie', 618);
 }
-*/
+
 
