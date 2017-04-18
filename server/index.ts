b7d1c9ecc1bc7299fc2a57a9f1ca71d24e3b9279
Nicholas Metzger <nicholas.metzger@tufts.edu>
2017-04-17 22:13:38 -0400

    request handling by adding secure routes

--- server/index.ts
+++ server/index.ts
@@ -2,8 +2,9 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as path from 'path';
 import * as database from './database';
-
+import { HttpMethod, RouteManager, InsecureRoute, InsecureRouteBuilder, SecureRoute, SecureRouteBuilder } from './utils/routeUtils';
 const app: express.Express = express();
+import { PermissionLevel } from './utils/requestUtils';
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -18,10 +19,31 @@ app.get('/test', (req, res) => {
 	res.send('ok');
 });
 
+const routeManager = new RouteManager(app);
+
+const tripFromCampusCreateBuilder = <SecureRouteBuilder>new SecureRouteBuilder('/Marilyn', (req, res, token) => {
+console.log("HI GRAHAM");
+}, PermissionLevel.DJ)
+        .setIsAjax(true)
+        .setHttpMethod(HttpMethod.POST);
+
+
+const fromCampusCreateRoutes: SecureRoute = new SecureRoute(tripFromCampusCreateBuilder);
+
+const secureRoutes = [
+
+    fromCampusCreateRoutes
+];
+
+routeManager.addSecureRoutes(secureRoutes);
+
 app.get('/*', (_, res: express.Response) => {
-    console.log(_);
+    console.log("Pikachu I choose you!");
     res.sendFile(path.resolve(`${clientDir}/../index.html`));
 
 });
 app.listen(port);
 console.log('ready to listen on', port);
+
+
+
