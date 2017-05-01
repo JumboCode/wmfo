272d4a7582fba6258a22ddda57fecf636528dd0f
Nicholas Metzger <nicholas.metzger@tufts.edu>
2017-04-30 22:14:44 -0400

    added insecure register route and began checking request body for bad input

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import * as database from './database';
 import { HttpMethod, RouteManager, InsecureRoute, InsecureRouteBuilder, SecureRoute, SecureRouteBuilder } from './utils/routeUtils';
 const app: express.Express = express();
-import { PermissionLevel } from './utils/requestUtils';
+import { PermissionLevel, badRequest } from './utils/requestUtils';
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -46,12 +46,38 @@ const loginBuilder = <InsecureRouteBuilder>new InsecureRouteBuilder('/login',
         .setIsAjax(true)
         .setHttpMethod(HttpMethod.GET);
 
+const registerBuilder = <InsecureRouteBuilder>new InsecureRouteBuilder('/register', 
+                                                          (req, res) => 
+{ console.log("You are now registering to be a DJ.");
+    var first_name = req.body.firstname;
+    var last_name = req.body.lastname;
+    var dj_name = req.body.djName;
+    var tuftsID = req.body.tuftsID;
+    var email = req.body.email;
+    var pw = req.body.password;
+    console.log(first_name);
+    console.log(last_name);
+    console.log(dj_name);
+    console.log(tuftsID);
+    console.log(email);
+    console.log(pw);
+  
+    if (!first_name || !last_name || !dj_name || !tuftsID || !email || !pw) 
+    {
+       badRequest(res, "You must fill in all required fields.", 400); 
+    } else { 
+        res.send("done registering!"); 
+    }
+}) 
+        .setIsAjax(true)
+        .setHttpMethod(HttpMethod.POST);
+
 
 const scheduleRoute: SecureRoute = new SecureRoute(scheduleBuilder);
 const addDJRoute: SecureRoute = new SecureRoute(addDJBuilder);
 const removeDJRoute: SecureRoute = new SecureRoute(removeDJBuilder);
 const loginRoute: InsecureRoute = new InsecureRoute(loginBuilder);
-
+const registerRoute: InsecureRoute = new InsecureRoute(registerBuilder);
 
 
 const secureRoutes = [
@@ -62,7 +88,8 @@ const secureRoutes = [
 
 
 const insecureRoutes = [
-    loginRoute
+    loginRoute,
+    registerRoute
 ];
 
 
